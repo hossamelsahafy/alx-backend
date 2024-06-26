083ac9cc86136463c2e95312329c17f53b4a8cbe
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-27 00:56:58 +0300

    File

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,9 +8,6 @@ from BaseCaching import BaseCaching
 class BasicCache(BaseCaching):
     """Define Basicache Class"""
 
-    def __init__(self):
-        """__init Method"""
-        super().__init__()
 
     def put(self, key, item):
         """Put Method"""
