158530aadd78fdbaa0ee625e20ccd7a83cb83c0f
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-06-27 03:25:22 +0300

    File

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -3,27 +3,27 @@
     LRU Caching
 """
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
-    """Define Class LRUCache Class"""
-
     def __init__(self):
         """__init__ Method"""
         super().__init__()
-        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Put Method"""
-        if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                last = next(iter(self.cache_data))
-                print("DISCARD: {}".format(last))
-                del self.cache_data[last]
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key)
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last = next(iter(self.cache_data))
+            del self.cache_data[last]
+            print(f"DISCARD: {last}")
 
     def get(self, key):
         """Get Method"""
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
