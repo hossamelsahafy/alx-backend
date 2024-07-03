412b643a45513fb0f8092f6429e9bbdfab60705e
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-03 15:16:58 +0300

    File

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,84 +1,23 @@
 #!/usr/bin/env python3
 """
-    Mock logging in
+    A Basic flask application
 """
 from flask import Flask, request, render_template, g
-from flask_babel import Babel
-from typing import Union, Dict
+from flask_babel import Babel, gettext
+from typing import (Dict, Union)
 
 
-# users = {
-#     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-#     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-#     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-#     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-# }
-
-
-# class Config(object):
-#     """
-#         Define Config class
-#     """
-#     LANGUAGES = ['en', 'fr']
-#     BABEL_DEFAULT_LOCALE = 'en'
-#     BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-# app = Flask(__name__)
-# app.config.from_object(Config)
-
-# babel = Babel(app)
-
-
-# # @babel.localeselector
-# def get_locale() -> str:
-#     """
-#         Gets locale from request object
-#     """
-#     locale = request.args.get('locale', '').strip()
-#     if locale and locale in Config.LANGUAGES:
-#         return locale
-#     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-# babel.locale_selector_func = get_locale
-
-
-# def get_user() -> Union[Dict[str, Union[str, None]], None]:
-#     """
-#         Returns a user dictionary or None if
-#         the ID cannot be found or if login_as was not passed.
-#     """
-#     try:
-#         user_id = int(request.args.get('login_as'))
-#         return users.get(user_id)
-#     except (TypeError, ValueError):
-#         return None
-
-
-# @app.before_request
-# def before_request():
-#     """
-#         Executes before each request
-#     """
-#     g.user = get_user()
-
-
-# @app.route('/', strict_slashes=False)
-# def index() -> str:
-#     """
-#         Renders a basic html template
-#     """
-#     return render_template('5-index.html')
-
-
-# if __name__ == '__main__':
-#     app.run()
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
     """
-        Define Config class
+    Define Config class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -91,10 +30,9 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localeselector
 def get_locale() -> str:
     """
-        Gets locale from request object
+    Gets locale from request object
     """
     locale = request.args.get('locale', '').strip()
     if locale and locale in Config.LANGUAGES:
@@ -102,19 +40,13 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
+babel.locale_selector_func = get_locale
 
 
-def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
+def get_user() -> Union[Dict[str, Union[str, None]], None]:
     """
-        Returns a user dictionary or None if
-        the ID cannot be found or if login_as
-        was not passed.
+    Returns a user dictionary or None if the
+    ID cannot be found or if login_as was not passed.
     """
     try:
         user_id = int(request.args.get('login_as'))
@@ -126,7 +58,7 @@ def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
 @app.before_request
 def before_request():
     """
-        Executes before each request
+    Executes before each request
     """
     g.user = get_user()
 
@@ -136,7 +68,7 @@ def index() -> str:
     """
     Renders a basic html template
     """
-    return render_template('5-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
