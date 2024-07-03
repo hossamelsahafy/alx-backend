6a0b251a72aebf1f00b68554d420ebe2f341b1ec
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-03 05:29:34 +0300

    File

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
 """
-    Basic Flask app
+A Basic flask application
 """
-from flask import Flask, render_template, request
+from flask import Flask
+from flask import request
+from flask import render_template
 from flask_babel import Babel
 
 
 class Config(object):
     """
-        Define Config Class
+    Application configuration class
     """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+# Instantiate the application object
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# Wrap the application with Babel
 babel = Babel(app)
 
 
+@babel.localeselector
 def get_locale() -> str:
     """
-        Method that returns the best match
+    Gets locale from request object
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel.locale_selector_func = get_locale
 
 
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-        Method that returns the template
+    Renders a basic html template
     """
     return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run
+if __name__ == '__main__':
+    app.run()
