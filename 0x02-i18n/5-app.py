3f6956377614e971eb567338cb7355ff46a2ce8d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-03 15:14:08 +0300

    File

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,23 +1,84 @@
 #!/usr/bin/env python3
 """
-A Basic flask application
+    Mock logging in
 """
 from flask import Flask, request, render_template, g
 from flask_babel import Babel
 from typing import Union, Dict
 
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
+# users = {
+#     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+#     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+#     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+#     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+# }
+
+
+# class Config(object):
+#     """
+#         Define Config class
+#     """
+#     LANGUAGES = ['en', 'fr']
+#     BABEL_DEFAULT_LOCALE = 'en'
+#     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+# app = Flask(__name__)
+# app.config.from_object(Config)
+
+# babel = Babel(app)
+
+
+# # @babel.localeselector
+# def get_locale() -> str:
+#     """
+#         Gets locale from request object
+#     """
+#     locale = request.args.get('locale', '').strip()
+#     if locale and locale in Config.LANGUAGES:
+#         return locale
+#     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+# babel.locale_selector_func = get_locale
+
+
+# def get_user() -> Union[Dict[str, Union[str, None]], None]:
+#     """
+#         Returns a user dictionary or None if
+#         the ID cannot be found or if login_as was not passed.
+#     """
+#     try:
+#         user_id = int(request.args.get('login_as'))
+#         return users.get(user_id)
+#     except (TypeError, ValueError):
+#         return None
+
+
+# @app.before_request
+# def before_request():
+#     """
+#         Executes before each request
+#     """
+#     g.user = get_user()
+
+
+# @app.route('/', strict_slashes=False)
+# def index() -> str:
+#     """
+#         Renders a basic html template
+#     """
+#     return render_template('5-index.html')
+
+
+# if __name__ == '__main__':
+#     app.run()
 
 
 class Config(object):
     """
-    Define Config class
+        Define Config class
     """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
@@ -30,7 +91,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale() -> str:
     """
         Gets locale from request object
@@ -41,13 +102,19 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.locale_selector_func = get_locale
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
-def get_user() -> Union[Dict[str, Union[str, None]], None]:
+def get_user(id) -> Union[Dict[str, Union[str, None]], None]:
     """
         Returns a user dictionary or None if
-        the ID cannot be found or if login_as was not passed.
+        the ID cannot be found or if login_as
+        was not passed.
     """
     try:
         user_id = int(request.args.get('login_as'))
@@ -67,7 +134,7 @@ def before_request():
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
-        Renders a basic html template
+    Renders a basic html template
     """
     return render_template('5-index.html')
 
