3fd717d973a7436c3a5c275965235d7653e37d9d
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-03 05:22:14 +0300

    File

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,20 +2,21 @@
 """
     Basic Flask app
 """
-
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-app = Flask(__name__)
 
-
-class Config:
+class Config(object):
+    """
+        Define Config Class
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
-
+app = Flask(__name__)
 app.config.from_object(Config)
+
 
 babel = Babel(app)
 
@@ -31,7 +32,7 @@ babel.locale_selector_func = get_locale
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """
         Method that returns the template
     """
@@ -39,4 +40,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run
