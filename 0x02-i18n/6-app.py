ccdf153ab80d12998aefcd48f62b2381e860aa19
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-03 15:56:30 +0300

    File

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """
-Mock logging in
+    Mock logging in
 """
 from flask import Flask, request, render_template, g
-from flask_babel import Babel, gettext
-from typing import Union, Dict, Optional
+from flask_babel import Babel
+from typing import Union, Dict
 
 
 users = {
@@ -14,6 +14,7 @@ users = {
     4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
 }
 
+
 class Config(object):
     """
         Define Config class
@@ -22,19 +23,17 @@ class Config(object):
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
 
+
+# @babel.localeselector
 def get_locale() -> str:
     """
-    Determines the best match for supported languages
-    based on the following priority:
-        1. Locale from URL parameters
-        2. Locale from user settings
-        3. Locale from request header
-        4. Default locale
+        Gets locale from request object
     """
     locale = request.args.get('locale', '').strip()
     if locale and locale in Config.LANGUAGES:
@@ -44,18 +43,21 @@ def get_locale() -> str:
         return user['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 babel.locale_selector_func = get_locale
 
-def get_user() -> Optional[Dict[str, Optional[str]]]:
+
+def get_user() -> Union[Dict[str, Union[str, None]], None]:
     """
-        Returns a user dictionary or None if the ID
-        cannot be found or if login_as was not passed.
+        Returns a user dictionary or None if
+        the ID cannot be found or if login_as was not passed.
     """
     try:
         user_id = int(request.args.get('login_as'))
         return users.get(user_id)
     except (TypeError, ValueError):
         return None
+
 
 @app.before_request
 def before_request():
@@ -64,12 +66,13 @@ def before_request():
     """
     g.user = get_user()
 
+
 @app.route('/', strict_slashes=False)
 def index() -> str:
     """
         Renders a basic html template
     """
-    return render_template('6-index.html')
+    return render_template('5-index.html')
 
 
 if __name__ == '__main__':
