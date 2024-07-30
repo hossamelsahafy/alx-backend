c7229af2edec28f1be9d4c2fedc4f6e0bb804875
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-30 19:54:04 +0300

    File

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,27 @@
+const redis = require('redis')
+const client = redis.createClient();
+client.on('conect', () => {
+    console.log("Redis client connected to the server")
+});
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`)
+});
+function createHash() {
+    client.hset('HolbertonSchools', 'Portland', 50, redis.print)
+    client.hset('HolbertonSchools', 'Seattle', 80, redis.print)
+    client.hset('HolbertonSchools', 'New York', 20, redis.print)
+    client.hset('HolbertonSchools', 'Bogota', 20, redis.print)
+    client.hset('HolbertonSchools', 'Cali', 40, redis.print)
+    client.hset('HolbertonSchools', 'Paris', 2, redis.print)
+}
+function displayHash() {
+    client.hgetall('HolbertonSchools', (err, reply) => {
+        if(err) {
+            console.log(`Error getting hash: ${err.message}`)
+        } else {
+            console.log(reply);
+        }
+    })
+}
+createHash();
+displayHash();
