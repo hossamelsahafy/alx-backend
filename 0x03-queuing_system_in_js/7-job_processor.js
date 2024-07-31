63512b0f7080ea66005f2db57b81580c49b04703
hossamelsahafy <hossamelsa7afy54@gmail.com>
2024-07-31 19:54:53 +0300

    Files

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,40 @@
+import kue from 'kue';
+
+const blacklistedNumbers = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  }
+
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
+
+const queue = kue.createQueue();
+
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
+});
+
+queue.on('job complete', (id, result) => {
+  kue.Job.get(id, (err, job) => {
+    if (err) return;
+    job.remove((err) => {
+      if (err) throw err;
+      console.log(`Notification job ${id} completed`);
+    });
+  });
+});
+
+queue.on('job failed', (id, errorMessage) => {
+  console.log(`Notification job ${id} failed: ${errorMessage}`);
+});
+
+queue.on('job progress', (id, progress) => {
+  console.log(`Notification job ${id} ${progress}% complete`);
+});
